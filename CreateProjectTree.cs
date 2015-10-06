a7feb81d6596d9c6771a10ebb8bbd3575db5cdc5
Daniel Koprowski <dkoprowski@outlook.com>
2015-10-06 23:54:52 +0200

    init - script generates only folders structure

---
+++ CreateProjectTree.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor;
+
+class MyFolderStruct
+{
+    public string Name;
+    public List<MyFolderStruct> Children;
+    public MyFolderStruct Parent;
+    public string Path { get { return GeneratePath(); } }
+    public MyFolderStruct(string name, MyFolderStruct parent)
+    {
+        Name = name;
+        Children = new List<MyFolderStruct>();
+        Parent = parent;
+    }
+
+    public MyFolderStruct()
+    {
+        Name = "";
+        Children = new List<MyFolderStruct>();
+    }
+
+    private string GeneratePath()
+    {
+        if (Parent != null)
+        {
+            return Parent.Path + "/" + Name;
+        }
+
+        return Name;
+    }
+}
+
+public class CreateProjectTree {
+
+    [MenuItem("Tools/Generate Project Tree")]
+    public static void Execute()
+    {
+        var assets = GenerateDirectory();
+        CreateFolders(assets);
+    }
+    //TODO: Secure against cycles. 
+    private static void CreateFolders(MyFolderStruct folders)
+    {
+        foreach (var folder in folders.Children)
+        {
+            if (!AssetDatabase.IsValidFolder(folder.Path) && folder.Parent != null)
+            {
+                Debug.Log("Creating: <b>" + folder.Path + "</b>");
+                AssetDatabase.CreateFolder(folder.Parent.Path, folder.Name);
+                //TODO: Add ".keep" files to empty folders for GIT
+            }
+            else
+            {
+                Debug.Log("Creating aborted: " + folder.Path);
+            }
+
+            if (folder.Children.Count > 0)
+            {
+                CreateFolders(folder);
+            }
+        }
+    }
+    private static MyFolderStruct GenerateDirectory()
+    {
+        MyFolderStruct assets = new MyFolderStruct() { Name = "Assets"};
+        assets.Children.Add(new MyFolderStruct("Scripts", assets));
+        assets.Children.Add(new MyFolderStruct("Scenes", assets));
+        assets.Children.Add(new MyFolderStruct("Extensions", assets));
+
+        var staticAssets = new MyFolderStruct("StaticAssets", assets);
+        staticAssets.Children.Add(new MyFolderStruct("Animations", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Animators", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Effects", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Fonts", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Materials", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Models", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Prefabs", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Shaders", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Sounds", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Sprites", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Textures", staticAssets));
+        staticAssets.Children.Add(new MyFolderStruct("Videos", staticAssets));
+
+        assets.Children.Add(staticAssets);
+        return assets;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+This script will generate basic folder structure for your Unity3D project.  
+Simply clone ```CreateProjectTree.cs``` into your ```Assets/Editor/*``` directory.
